92148c6ce32d8909ad099006760dabeed4032ff7
priyankodey <priyankodey87@gmail.com>
2014-08-14 12:31:43 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RupeeVest::Application.config.secret_key_base = 'db7e04e6c96bd8f66245950e89311bb02863f0911e1925a95048eb24a03502190a28d5a615626f32c1dad6aadfc5fe29f1474536c8065ba64a75507ff4dea5c2'
